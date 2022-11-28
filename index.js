079451177ca3562ea258a415fe837b0807ea9285
jeanl7281 <jeanl.03.2007@gmail.com>
2022-11-27 19:30:38 -0500

    code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const DOMselectors = {
     button: document.querySelector(`#btn1`),
     button2: document.querySelector(`#btn2`),
@@ -39,52 +39,3 @@ const DOMselectors = {
       });
     });
   }
-=======
-DOMSelectors = {
-  form: document.getElementById("form"),
-  animal: document.getElementById("animal"),
-  type: document.getElementById("type"),
-  image: document.getElementById("image"),
-  output: document.getElementById("output"),
-};
-
-DOMSelectors.form.addEventListener("submit", function () {
-  event.preventDefault();
-  element();
-  removebutton();
-  reset();
-});
-
-function enterFunction(event) {
-  event.preventDefault();
-  console.log("hello");
-  descript = DOMSelectors.description.value;
-  title = DOMSelectors.name.value;
-  image = DOMSelectors.image.value;
-  DOMSelectors.output.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="output">
-                <h2>${title}</h2>
-                <p>${descript}</p>
-                <img class="ouput-image" src="${image}">
-                <br>
-                <button class="button">Remove</button>
-        </div>`
-  );
-}
-
-function removebutton() {
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      this.parentElement.remove();
-    });
-  });
-}
-
-function reset() {
-  DOMSelectors.description.value = "";
-  DOMSelectors.name.value = "";
-  DOMSelectors.image.value = "";
-}
->>>>>>> 262231864502e01c93c82bb5f9b1b6b90ec43780
